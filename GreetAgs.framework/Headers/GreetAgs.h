a700cf0ba80666c72c8d9a8230e92b065b430d76
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-16 17:52:14 +0530

    Intial commit

---
+++ GreetAgs.framework/Headers/GreetAgs.h
@@ -0,0 +1,18 @@
+//
+//  GreetAgs.h
+//  GreetAgs
+//
+//  Created by IOS on 16/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GreetAgs.
+FOUNDATION_EXPORT double GreetAgsVersionNumber;
+
+//! Project version string for GreetAgs.
+FOUNDATION_EXPORT const unsigned char GreetAgsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreetAgs/PublicHeader.h>
+
+
